5d954928d7badb8445527969165a7730d664bd51
pretty-player <geekforlinux999@gmail.com>
2026-01-29 23:31:12 +0530

    changed file content

---
+++ pythontask2/task2A.py
@@ -0,0 +1,8 @@
+# List with duplicate numbers
+raw_list = [10, 20, 20, 30, 40, 40, 50]
+
+# Convert list to a set to remove duplicates
+unique_set = set(raw_list)
+
+print(f"Original List: {raw_list}")
+print(f"Set (Duplicates Removed): {unique_set}")
